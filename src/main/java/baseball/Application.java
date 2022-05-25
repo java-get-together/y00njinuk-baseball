729eec6bfdce8791c08eb5de57bf6bf900146e81
棟幹 尹鎭煜 <71953982+y00njinuk@users.noreply.github.com>
2022-05-26 04:15:48 +0900

    feat: add method that convert byte array to int array

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,4 +4,15 @@ public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
     }
+
+    private static int[] toIntArray(byte[] bArray) {
+        if(bArray.length > 3)
+            throw new IllegalArgumentException();
+
+        int[] temp = new int[bArray.length];
+        for (int i = 0; i < temp.length; i++)
+            temp[i] = bArray[i] - '0';
+
+        return temp;
+    }
 }
