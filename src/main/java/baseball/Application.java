20648359d6f2218df6d3a49fc4333ce04d5ad6ca
棟幹 尹鎭煜 <71953982+y00njinuk@users.noreply.github.com>
2022-05-26 04:16:30 +0900

    feat: add method that compare with target number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
@@ -14,5 +16,35 @@ public class Application {
             temp[i] = bArray[i] - '0';
 
         return temp;
+    }
+    private static boolean compareWith(Map<Integer, Integer> numMap, int[] numArray) {
+        int strike = 0;
+        int ball = 0;
+
+        for(int i = 0; i < numArray.length; i ++) {
+            if (numMap.containsKey(numArray[i])) {
+                if (numMap.get(numArray[i]) == i)
+                    strike++;
+                else
+                    ball++;
+            }
+        }
+
+        if (strike == 0 && ball == 0)
+            System.out.println("낫싱");
+        else if (strike == 0)
+            System.out.printf("%d볼\n", ball);
+        else if (ball == 0)
+            System.out.printf("%d스트라이크\n", strike);
+        else
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+
+        if(strike == numArray.length) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
+        }
+        else
+            return false;
     }
 }
